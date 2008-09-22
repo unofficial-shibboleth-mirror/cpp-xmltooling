e02ccbda2e181771cb555649ffd25958594b37dc
Scott Cantor <cantor.2@osu.edu>
2008-09-22 19:51:21 +0000

    Bump version to 1.2.

--- config_win32.h
+++ config_win32.h
@@ -75,13 +75,13 @@
 #define PACKAGE_NAME "xmltooling"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xmltooling 1.1"
+#define PACKAGE_STRING "xmltooling 1.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xmltooling"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1"
+#define PACKAGE_VERSION "1.2"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -94,7 +94,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "1.1"
+#define VERSION "1.2"
 
 /* Define if you wish to disable XML-Security-dependent features. */
 /* #undef XMLTOOLING_NO_XMLSEC */
