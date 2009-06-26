ff7f68e4571783d23d0418973adf657eb81223c7
Scott Cantor <cantor.2@osu.edu>
2009-06-26 03:39:07 +0000

    Bump revision in case a patch release is done.

--- config_win32.h
+++ config_win32.h
@@ -87,13 +87,13 @@
 #define PACKAGE_NAME "xmltooling"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xmltooling 1.2"
+#define PACKAGE_STRING "xmltooling 1.2.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xmltooling"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2"
+#define PACKAGE_VERSION "1.2.1"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -106,7 +106,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "1.2"
+#define VERSION "1.2.1"
 
 /* Define if you wish to disable XML-Security-dependent features. */
 /* #undef XMLTOOLING_NO_XMLSEC */
