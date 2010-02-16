f4fa308b76a6ee6231e593729d81bde5269c0ca1
Scott Cantor <cantor.2@osu.edu>
2010-02-16 18:36:08 +0000

    https://issues.shibboleth.net/jira/browse/CPPXT-55

--- xmltooling/security/impl/BasicX509Credential.cpp
+++ xmltooling/security/impl/BasicX509Credential.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2001-2009 Internet2
+ *  Copyright 2001-2010 Internet2
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -343,7 +343,7 @@ void BasicX509Credential::extract()
         char* serial = BN_bn2dec(serialBN);
         if (serial) {
             m_serial = serial;
-            free(serial);
+            OPENSSL_free(serial);
         }
         BN_free(serialBN);
     }
