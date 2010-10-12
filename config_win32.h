aad3cc122bf1227d27418b072c9bf91a47152fe6
Scott Cantor <cantor.2@osu.edu>
2010-10-12 15:48:41 +0000

    Start revisions of autotools material.

--- config_win32.h
+++ config_win32.h
@@ -91,7 +91,7 @@
 #define PACKAGE "xmltooling"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "mace-opensaml-users@internet2.edu"
+#define PACKAGE_BUGREPORT "https://bugs.internet2.edu/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "xmltooling"
