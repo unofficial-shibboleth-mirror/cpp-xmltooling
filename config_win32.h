ef34378810ae8079b6ba34532ab76f16e56a24d7
Scott Cantor <cantor.2@osu.edu>
2011-06-16 13:34:04 +0000

    Remove pkg files from configure, fix bug link

--- config_win32.h
+++ config_win32.h
@@ -108,7 +108,7 @@
 #define PACKAGE "xmltooling"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://bugs.internet2.edu/"
+#define PACKAGE_BUGREPORT "https://issues.shibboleth.net/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "xmltooling"
