ba80fe4167b68359c8f6c90d1f9cd55465b4c0c7
Scott Cantor <cantor.2@osu.edu>
2015-01-09 04:30:39 +0000

    https://issues.shibboleth.net/jira/browse/CPPXT-99

--- xmltooling/soap/impl/CURLSOAPTransport.cpp
+++ xmltooling/soap/impl/CURLSOAPTransport.cpp
@@ -535,11 +535,11 @@ void CURLSOAPTransport::send(istream* in)
     m_useragent = XMLToolingConfig::getConfig().user_agent;
     if (!m_useragent.empty()) {
         curl_version_info_data* curlver = curl_version_info(CURLVERSION_NOW);
-        m_useragent += " libcurl/";
-        if (curlver)
-            m_useragent = m_useragent + curlver->version + ' ' + curlver->ssl_version;
-        else
-            m_useragent = m_useragent + LIBCURL_VERSION + ' ' + OPENSSL_VERSION_TEXT;
+
+        if (curlver) {
+            m_useragent = m_useragent + " libcurl/" + curlver->version + ' ' + curlver->ssl_version;
+        }
+
         curl_easy_setopt(m_handle, CURLOPT_USERAGENT, m_useragent.c_str());
     }
 
