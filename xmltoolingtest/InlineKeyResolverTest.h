a673915585ca04c71f8330857472ad8233dbe3ea
Rod Widdowson <rdw@steadingsoftware.com>
2016-11-01 16:35:27 +0000

    CPPXT-110 Add test path for RSA loadXXXBigNums
    https://issues.shibboleth.net/jira/browse/CPPXT-110
    
    This completes the code coervage tests for the OpenSSLCryptoKeyRSA
    changes for OpenSSL 1.1
    
    Add a test function to force the load of the RSA key from the
    <ds:RSAKeyValue>, then cast it and the credential into the
    base OpenSSL type.
    
    Throw various OpenSSL incantations at these to extract the OpenSSL
    struct and compare the two.  This will need work when we move to
    OpenSSL 1.1 (since the structures become opaque).

--- xmltoolingtest/InlineKeyResolverTest.h
+++ xmltoolingtest/InlineKeyResolverTest.h
@@ -21,10 +21,28 @@
 #include "XMLObjectBaseTestCase.h"
 
 #include <fstream>
+
 #include <xmltooling/security/X509Credential.h>
 #include <xmltooling/security/KeyInfoResolver.h>
 #include <xmltooling/signature/KeyInfo.h>
+
 #include <xsec/enc/XSECCryptoKey.hpp>
+
+#include <xsec/dsig/DSIGReference.hpp>
+#include <xsec/dsig/DSIGSignature.hpp>
+extern "C" {
+#include <openssl/opensslv.h>
+}
+
+// Force XMLSEC to assume OpenSSL
+#define XSEC_HAVE_OPENSSL 1
+
+#include <xsec/enc/OpenSSL/OpenSSLCryptoX509.hpp>
+#include <xsec/enc/OpenSSL/OpenSSLCryptoKeyDSA.hpp>
+#include <xsec/enc/OpenSSL/OpenSSLCryptoKeyEC.hpp>
+#include <xsec/enc/OpenSSL/OpenSSLCryptoKeyRSA.hpp>
+
+
 
 using namespace xmlsignature;
 
@@ -63,6 +81,54 @@ public:
         TSM_ASSERT_EQUALS("Unexpected key type.", cred->getPublicKey()->getKeyType(), XSECCryptoKey::KEY_RSA_PUBLIC);
         TSM_ASSERT_EQUALS("Wrong certificate count.", cred->getEntityCertificateChain().size(), 1);
         TSM_ASSERT_EQUALS("Wrong CRL count.", cred->getCRLs().size(), 3);
+    }
+
+
+    void testOpenSSLRSA() {
+        string path=data_path + "KeyInfo1.xml";
+        ifstream fs(path.c_str());
+        DOMDocument* doc=XMLToolingConfig::getConfig().getValidatingParser().parse(fs);
+        TS_ASSERT(doc!=nullptr);
+        const XMLObjectBuilder* b = XMLObjectBuilder::getBuilder(doc->getDocumentElement());
+        TS_ASSERT(b!=nullptr);
+        auto_ptr<KeyInfo> kiObject(dynamic_cast<KeyInfo*>(b->buildFromDocument(doc)));
+        TS_ASSERT(kiObject.get()!=nullptr);
+
+        auto_ptr<X509Credential> cred(dynamic_cast<X509Credential*>(m_resolver->resolve(kiObject.get())));
+        auto_ptr<X509Credential> key(dynamic_cast<X509Credential*>(m_resolver->resolve(kiObject.get(), Credential::RESOLVE_KEYS)));
+
+        OpenSSLCryptoKeyRSA* sslCred = dynamic_cast<OpenSSLCryptoKeyRSA*>(cred->getPublicKey());
+        OpenSSLCryptoKeyRSA* sslKey = dynamic_cast<OpenSSLCryptoKeyRSA*>(key->getPublicKey());
+
+        RSA* rsaCred = sslCred->getOpenSSLRSA();
+        RSA* rsaKey = sslKey->getOpenSSLRSA();
+
+        BIGNUM* n = rsaCred->n;
+        BIGNUM* e = rsaCred->e;
+        BIGNUM* d = rsaCred->d;
+        BIGNUM* p = rsaCred->p;
+        BIGNUM* q = rsaCred->q;
+        BIGNUM* dmp1 = rsaCred->dmp1;
+        BIGNUM* dmq1 = rsaCred->dmq1;
+        BIGNUM* iqmp = rsaCred->iqmp;
+
+        BIGNUM* kn = rsaKey->n;
+        BIGNUM* ke = rsaKey->e;
+        BIGNUM* kd = rsaKey->d;
+        BIGNUM* kp = rsaKey->p;
+        BIGNUM* kq = rsaKey->q;
+        BIGNUM* kdmp1 = rsaKey->dmp1;
+        BIGNUM* kdmq1 = rsaKey->dmq1;
+        BIGNUM* kiqmp = rsaKey->iqmp;
+
+        TS_ASSERT(0 == BN_cmp(kn, n));
+        TS_ASSERT(0 == BN_cmp(ke, e));
+        TS_ASSERT(0 ==  BN_cmp(kd, d));
+        TS_ASSERT(0 ==  BN_cmp(kp, p));
+        TS_ASSERT(0 == BN_cmp(kq, q));
+        TS_ASSERT(0 ==  BN_cmp(kdmp1, dmp1));
+        TS_ASSERT(0 ==  BN_cmp(kdmq1, dmq1));
+        TS_ASSERT(0 ==  BN_cmp(kiqmp, iqmp));
     }
 
     void testDER() {
